9709f0d4585b18c6e2eff7b1fe44d79f3ce26ba0
Vivian Zhang <vvynz12@gmail.com>
2022-01-12 02:12:19 +0000

    function checks if 2 arrays are equal and passed through an assertion function which will return true if all elements of both arrays are equal and false if not

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arrA, arrB) {
+  if (arrA.every((val, index) => val === arrB[index])) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
