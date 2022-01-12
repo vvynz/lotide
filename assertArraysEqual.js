dde58448c5c37b71bc8a86708bec3c60da8dc7a3
Vivian Zhang <vvynz12@gmail.com>
2022-01-12 19:32:10 +0000

    refactored the eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,18 @@
 const eqArrays = function (arrA, arrB) {
-  let maxLength = arrA.length > arrB.length ? arrA.length : arrB.length;
-  if (arrA.length === maxLength && arrB.length === maxLength) {
-    for (let i = 0; i < maxLength; i++) {
-      if (arrA[i] > arrB[i]) {
-        console.log(arrA[i], arrB[i]);
-        return false;
-      } else {
-        return true;
-      }
-    }
-  } else {
+  if (arrA.length !== arrB.length) {
     return false;
   }
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      console.log(arrA[i], arrB[i]);
+      return false;
+    }
+  }
+  return true;
 };
 
 const assertArraysEqual = function (actual, expected) {
-  //console.log(arrTwo);
+  //console.log()
   if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   } else {
--- eqArrays.js
+++ eqArrays.js
@@ -7,26 +7,23 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = function (arrA, arrB) {
-  let maxLength = arrA.length > arrB.length ? arrA.length : arrB.length;
-  if (arrA.length === maxLength && arrB.length === maxLength) {
-    for (let i = 0; i < maxLength; i++) {
-      if (arrA[i] > arrB[i]) {
-        console.log(arrA[i], arrB[i]);
-        return false;
-      } else {
-        return true;
-      }
-    }
-  } else {
+  if (arrA.length !== arrB.length) {
     return false;
   }
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      console.log(arrA[i], arrB[i]);
+      return false;
+    }
+  }
+  return true;
+};
 
-  /*if (arrA.every((val, index) => val === arrB[index])) {
+/*if (arrA.every((val, index) => val === arrB[index])) {
     return true;
   } else {
     return false;
   }*/
-};
 
 eqArrays([1, 2, 3], [1, 2, 3]);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
