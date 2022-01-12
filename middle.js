5438988dd674ddf8c965143614ca59acb6b9ed58
Vivian Zhang <vvynz12@gmail.com>
2022-01-12 18:50:39 +0000

    a function that returns 1 or 2 middle elements of an array

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function (arrA, arrB) {
+  if (arrA.length !== arrB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      console.log(arrA[i], arrB[i]);
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  //console.log()
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  }
+};
+
+const middle = function (arr) {
+  if (arr.length <= 2) {
+    return [];
+  }
+  const middleIndex = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    return [arr[middleIndex - 1], arr[middleIndex]];
+  } else {
+    return [arr[middleIndex]];
+  }
+};
+
+assertArraysEqual(eqArrays(middle([1]), []), true);
+assertArraysEqual(eqArrays(middle([1, 2]), []), true);
+assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true);
+assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
+assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6, 7]), [4]), true);
