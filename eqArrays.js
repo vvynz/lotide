ca1dee56fa4f1152c5d7595e85e1695420344cf6
Vivian Zhang <vvynz12@gmail.com>
2022-01-12 05:21:48 +0000

    changed the function to a nested array to compare the 2 arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,13 +7,29 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = function (arrA, arrB) {
-  if (arrA.every((val, index) => val === arrB[index])) {
-    return true;
+  let maxLength = arrA.length > arrB.length ? arrA.length : arrB.length;
+  if (arrA.length === maxLength && arrB.length === maxLength) {
+    for (let i = 0; i < arrA.length; i++) {
+      for (let j = 0; j < arrB.length; j++) {
+        if (arrA[i] === arrB[j]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
   } else {
     return false;
   }
+
+  /*if (arrA.every((val, index) => val === arrB[index])) {
+    return true;
+  } else {
+    return false;
+  }*/
 };
 
+eqArrays([1, 2, 3], [1, 2, 3]);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
