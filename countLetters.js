a52f57d2d97c5d4693c73dbbc276196acb250228
Vivian Zhang <vvynz12@gmail.com>
2022-01-12 23:14:48 +0000

    skipped spaces in the string parameters & refactored the code

--- countLetters.js
+++ countLetters.js
@@ -1,24 +1,12 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const countLetters = function (strResult, countResult) {
+const countLetters = function (strMessage) {
   const result = {};
 
-  for (const label of strResult) {
-    result[label] = 0;
-    if (countResult[label]) {
-      for (let i = 0; i < strResult.length; i++) {
-        if (strResult[i] === label) {
-          result[label] += 1;
-        }
-        if (strResult[i] === " ") {
-          strResult[i].slice(0);
-        }
+  for (const label of strMessage) {
+    if (label !== " ") {
+      if (result[label]) {
+        result[label] += 1;
+      } else {
+        result[label] = 1;
       }
     }
   }
@@ -26,4 +14,4 @@ const countLetters = function (strResult, countResult) {
   return result;
 };
 
-assertEqual(countLetters("lighthouse in the house"), []);
+countLetters("lighthouse in the house");
