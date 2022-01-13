dbe7be5a4b486d84980c4e7f4d46e8239d88375c
Vivian Zhang <vvynz12@gmail.com>
2022-01-13 22:11:29 +0000

    created own version of the map function

---
+++ map.js
@@ -0,0 +1,39 @@
+const eqArrays = function (arrA, arrB) {
+  if (arrA.length !== arrB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      console.log(arrA[i], arrB[i]);
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  //console.log()
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+//console.log(results);
+
+const map = function (array, callback) {
+  const result = [];
+
+  for (let item of array) {
+    result.push(callback(item));
+  }
+  return result;
+};
+const results1 = (words, (word) => word[0]);
+assertArraysEqual(
+  eqArrays(map(words, results1), ["g", "c", "t", "m", "t"]),
+  true
+);
