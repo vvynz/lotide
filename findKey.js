b295560f89e25c0eb0df9b443f41d7c88148c45b
Vivian Zhang <vvynz12@gmail.com>
2022-01-16 20:48:50 +0000

    function returns an undefined when no key is found

--- findKey.js
+++ findKey.js
@@ -7,31 +7,44 @@ const assertEqual = function (actual, expected) {
 };
 
 const findKey = function (obj, callback) {
-  const v = Object.values(obj);
+  // const v = Object.values(obj);
   // returns an array of object of the values i.e. [{ stars: 1 },...]
   // console.log(v[0]);
 
-  for (let i = 0; i < v.length; i++) {
-    // console.log(v[i]);
-    if (obj[i] === callback[key]) {
-      console.log(callback(key, i));
+  // for (let i = 0; i < v.length; i++) {
+  //   // console.log(v[i]);
+  //   if (obj[i] === callback[key]) {
+  //     console.log(callback(key, i));
+  //   }
+  // }
+
+  for (let key in obj) {
+    if (!obj.hasOwnProperty(key)) {
+      return undefined;
+    }
+
+    if (obj.hasOwnProperty(key)) {
+      console.log(obj[key]);
+      if (obj[key] === callback[key]) {
+        console.log(callback[key]);
+      }
     }
   }
 };
 
-console.log(
-  findKey(
-    {
-      "Blue Hill": { stars: 1 },
-      Akaleri: { stars: 3 },
-      noma: { stars: 2 },
-      elBulli: { stars: 3 },
-      Ora: { stars: 2 },
-      Akelarre: { stars: 3 },
-    },
-    (x) => x.stars === 2
-  )
-);
+// console.log(
+//   findKey(
+//     {
+//       "Blue Hill": { stars: 1 },
+//       Akaleri: { stars: 3 },
+//       noma: { stars: 2 },
+//       elBulli: { stars: 3 },
+//       Ora: { stars: 2 },
+//       Akelarre: { stars: 3 },
+//     },
+//     (x) => x.stars === 2
+//   )
+// );
 
 assertEqual(
   findKey(
