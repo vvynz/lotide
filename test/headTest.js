09437a0bab87ef5a82656d7e20f1090ccd8a346b
Vivian Zhang <vvynz12@gmail.com>
2022-01-17 20:31:02 +0000

    installed Mocha and Chai to our testing folder. Now using Mocha's functions in our headTest.js to register our tests.

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,18 @@
+const assert = require("chai").assert;
 const head = require("../head");
 const assertEqual = require("../assertEqual");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["MYG"]), "MYG");
-assertEqual(head([]), 1);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns 'MYG' for ['MYG']", () => {
+    assert.strictEqual(head(["MYG"]), "MYG");
+  });
+});
