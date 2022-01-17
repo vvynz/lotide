19c61857f8ccbb60736bd7a1114f0711c81225f3
Vivian Zhang <vvynz12@gmail.com>
2022-01-17 18:17:00 +0000

    refactored the codes in both files to export the functions from their modules. Also moved the test codes into separate files and made sure the test file would run, by requiring the functions from the other files/ modules.

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 10);
-assertEqual("MYG", "JJk");
-assertEqual(110, 110);
-assertEqual("YOONGI", "YOONGI");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 10);
+assertEqual("MYG", "JJk");
+assertEqual(110, 110);
+assertEqual("YOONGI", "YOONGI");
