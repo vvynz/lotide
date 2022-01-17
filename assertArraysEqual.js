c3ec72f0589e71ad62cd454c63be71269bb4b142
Vivian Zhang <vvynz12@gmail.com>
2022-01-17 19:13:23 +0000

    refactored the assertArraysEqual.js. The moduleexports its assertArraysEqual function. A separate test file was created and require the eqArrays and assertArraysEqual functions. Test run smoothly!

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,3 @@
-const eqArrays = function (arrA, arrB) {
-  if (arrA.length !== arrB.length) {
-    return false;
-  }
-  for (let i = 0; i < arrA.length; i++) {
-    if (arrA[i] !== arrB[i]) {
-      console.log(arrA[i], arrB[i]);
-      return false;
-    }
-  }
-  return true;
-};
-
 const assertArraysEqual = function (actual, expected) {
   //console.log()
   if (actual !== expected) {
@@ -20,7 +7,4 @@ const assertArraysEqual = function (actual, expected) {
   }
 };
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require("../eqArrays");
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
