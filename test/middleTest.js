d2a9d06e162ea1479a06ed4faf07cfae4e8862ba
Vivian Zhang <vvynz12@gmail.com>
2022-01-17 22:43:00 +0000

    refactored the code to run the tests using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,33 @@
+const expect = require("chai").expect;
 const assertArraysEqual = require("../assertArraysEqual");
 const eqArrays = require("../eqArrays");
 const middle = require("../middle");
 
-assertArraysEqual(eqArrays(middle([1]), []), true);
-assertArraysEqual(eqArrays(middle([1, 2]), []), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6, 7]), [4]), true);
+// assertArraysEqual(eqArrays(middle([1]), []), true);
+// assertArraysEqual(eqArrays(middle([1, 2]), []), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6, 7]), [4]), true);
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    const num = [1];
+    const result = middle(num);
+    const expectedResult = [];
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    const num = [1, 2, 3, 4, 5];
+    const result = middle(num);
+    const expectedResult = [3];
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    const num = [1, 2, 3, 4, 5, 6];
+    const result = middle(num);
+    const expectedResult = [3, 4];
+    expect(result).to.deep.equal(expectedResult);
+  });
+});
