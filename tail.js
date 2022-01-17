1533837b3dbe57f5f05bf0fea87e89bf035d72ee
Vivian Zhang <vvynz12@gmail.com>
2022-01-17 22:36:57 +0000

    refactored tail.js and tailTest.js to run the tests with mocha and chai.

--- tail.js
+++ tail.js
@@ -1,5 +1,5 @@
 const tail = function (array) {
-  console.log(array.slice(1));
+  return array.slice(1);
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,35 @@
+const expect = require("chai").expect;
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
-const words = ["BTS", "ARMY", "BORAHAE"];
+const words = ["Hi", "hello", "bye"];
 
-tail(words);
-assertEqual(words.length, 3);
+// tail(words);
+// assertEqual(words.length, 3);
 
-tail([100]);
-assertEqual(tail.length, 1);
+// tail([100]);
+// assertEqual(tail.length, 1);
 
-tail([]);
-assertEqual(tail.length, 1);
+// tail([]);
+// assertEqual(tail.length, 1);
+
+describe("#tail", () => {
+  it("returns ['hello', 'bye'] for ['Hi', 'hello', 'bye']", () => {
+    const words = ["Hi", "hello", "bye"];
+    const result = tail(words);
+    const expectedResult = ["hello", "bye"];
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it("returns 1 for []", () => {
+    const emptyArr = [];
+    const result = tail([]);
+    const expectedResult = [];
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it("returns 100 for [100]", () => {
+    const numArr = [100];
+    const result = tail([100]);
+    const expectedResult = [];
+    expect(result).to.deep.equal(expectedResult);
+  });
+});
